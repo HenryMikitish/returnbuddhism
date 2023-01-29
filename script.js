95d900e4c9a84fe69ffae0a2ee22d1faed183c55
HenryMikitish <henry.mikitish@gmail.com>
2023-01-28 16:09:26 -0800

    Make retro shrine color properly again

--- script.js
+++ script.js
@@ -26,3 +26,32 @@ function hide(e) {
 
 topics.forEach(topic => topic.addEventListener('mouseover', unhide));
 links.forEach(link => link.addEventListener('mouseleave', hide));
+
+//retro
+
+const retroLink = document.querySelector('.retro-link');
+const retroTopic = document.querySelector('.retro-topic');
+const retroDesc = document.querySelector('.retro-desc');
+const retroOne = document.querySelector('#retro-one');
+const retroTwo = document.querySelector('#retro-two');
+
+
+retroTopic.addEventListener('mouseover', function() {
+    retroDesc.classList.remove('hidden');
+    retroDesc.classList.add('description');
+    retroLink.classList.add('button');
+    retroOne.classList.add('color-retro-one');
+    retroTwo.classList.add('color-retro-two');
+
+    $(".retro-link").click(function() {
+        window.location = $(this).find("a").attr("href"); 
+        return false;
+      });
+});
+
+retroLink.addEventListener('mouseleave', function() {
+    retroDesc.classList.add('hidden');
+    retroLink.classList.remove('button');
+    retroOne.classList.remove('color-retro-one');
+    retroTwo.classList.remove('color-retro-two');
+});
